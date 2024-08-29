e6df3a4106468ad9d2a7bae8a6648ff438e267fa
Yin Xun <kho.yinxun@gmail.com>
2024-08-29 21:41:58 +0800

    a new branch

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("I printed something");
+        System.out.println("now this is a new branch");
     }
 }
