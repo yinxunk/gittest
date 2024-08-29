5d1c3666fd06b047fcd34989b80c6cbca1d7bf62
Yin Xun <kho.yinxun@gmail.com>
2024-08-29 22:16:31 +0800

    solved ds_store

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("I printed something");
-        System.out.println("now this is a new branch");
     }
 }
